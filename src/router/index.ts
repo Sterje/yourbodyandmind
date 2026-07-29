d8cfaee3463a862467b81789550712db72fc456c
Jesper Eriksson Stenberg <jespermail@gmail.com>
2026-07-29 12:55:20 +0200

    import in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Start from "../pages/mobile/Start.vue";
+import Start from "../pages/mobile/StartMobile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
